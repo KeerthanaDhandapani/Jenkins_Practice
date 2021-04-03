8fa857bb5a4ae8674ea8993b256542f1c9abc0d8
KeerthanaDhandapani <74096537+KeerthanaDhandapani@users.noreply.github.com>
2021-04-03 20:44:13 +0530

    Update ThemeParkRideController.java

--- src/main/java/com/tg/controller/ThemeParkRideController.java
+++ src/main/java/com/tg/controller/ThemeParkRideController.java
@@ -26,20 +26,16 @@ public class ThemeParkRideController {
     public ThemeParkRide getRide(@PathVariable long id){
         return themeParkRideRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Invalid ride id %s", id)));
     }
-<<<<<<< HEAD
-=======
-    
-      
-@PostMapping(value = "/ride", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ThemeParkRide createRide(@Valid @RequestBody ThemeParkRide themeParkRide) {
-        return themeParkRideRepository.save(themeParkRide);
-    }
 
-@DeleteMapping(value = "/ride/{id}")
-    public void deleteRide(@PathVariable long id){
-        themeParkRideRepository.deleteById(id);
-    }
+      
+// @PostMapping(value = "/ride", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+//     public ThemeParkRide createRide(@Valid @RequestBody ThemeParkRide themeParkRide) {
+//         return themeParkRideRepository.save(themeParkRide);
+//     }
+
+// @DeleteMapping(value = "/ride/{id}")
+//     public void deleteRide(@PathVariable long id){
+//         themeParkRideRepository.deleteById(id);
+//     }
     
-    
->>>>>>> 5e50331f8eab40ad9bd5140bd6273bd463d5f1ee
 }
